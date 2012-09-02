eede7cd2395653e8f4324f6263a96458efc6a0cb
ume540 <masa.umeda@gmail.com>
2012-09-02 20:12:01 +0900

    add some files

---
+++ android/setup-build-env.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#
+# ubuntu 10.04 LTS 64bit
+#
+
+# TODO: install git 1.7 or later
+
+# install required packages
+sudo apt-get install \
+    gnupg \
+    flex \
+    bison \
+    gperf \
+    build-essential \
+    zip \
+    curl \
+    zlib1g-dev \
+    libc6-dev \
+    lib32ncurses5-dev \
+    ia32-libs \
+    x11proto-core-dev \
+    libx11-dev \
+    lib32readline5-dev \
+    lib32z-dev \
+    libgl1-mesa-dev \
+    g++-multilib \
+    mingw32 \
+    tofrodos \
+    python-markdown \
+    libxml2-utils \
+    xsltproc \
+
+
