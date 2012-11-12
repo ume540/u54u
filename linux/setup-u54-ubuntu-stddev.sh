0eb0ceaf03b8a2c4cabda7ca315b4e7ca9f8e2f2
Masayuki Umeda <umeda@ubuntu.(none)>
2012-11-12 10:58:46 +0900

    Add buildroot setup script.

--- linux/setup-u54-ubuntu-stddev.sh
+++ linux/setup-u54-ubuntu-stddev.sh
@@ -2,9 +2,24 @@
 
 sudo apt-get update
 sudo apt-get install \
-    exuberant-ctags global \
+    exuberant-ctags \
+    global \
     nkf \
-    git-core mercurial subversion \
-    gcc make build-essential \
-    zip curl fakeroot \
+    meld \
+    git-core \
+    mercurial \
+    subversion \
+    zip \
+    curl \
+    fakeroot \
+    gcc \
+    make \
+    build-essential \
+    gettext \
+    texinfo \
+    autoconf \
+    automake \
+    libtool \
+    pkg-config \
+    gperf \
 
